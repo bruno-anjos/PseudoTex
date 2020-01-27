a4a9879c60bf812d4a9f7f80ecbd547355f44aa0
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-01-27 23:21:14 +0000

    Add main code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+use std::io::prelude::*;
+use my_parser::Translate;
+
+#[macro_use]
+extern crate lalrpop_util;
+
+lalrpop_mod!(pub parser);
+
+#[macro_use] mod latex;
+mod my_parser;
+mod values;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    for line in io::stdin().lock().lines() {
+        input.push_str(line.unwrap().as_str());
+        input.push('\n');
+    }
+
+    let result: String = parser::MainParser::new().parse(input.as_str())
+        .unwrap()
+        .eval_translate();
+
+    println!("{}", result);
+
+    let mut file = std::fs::File::create("demo.tex").expect("create failed");
+    file.write_all(format!("{}\n{}\n{}", BEGIN!(), result, END!()).as_bytes())
+        .expect("write failed");
 }
